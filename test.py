d5bfda405216b82fdc6b4c515d17964ae6c707af
Artur Pedro <arturpedromartins@gmail.com>
2022-11-15 16:41:29 -0300

    funções básicas da class Termo e outras funções

--- test.py
+++ test.py
@@ -1,26 +1,43 @@
+def merge_dicts(*dict_args):
+    """
+    Given any number of dictionaries, shallow copy and merge into a new dict,
+    precedence goes to key-value pairs in latter dictionaries.
+    """
+    result = {}
+    for d in dict_args:
+        for k in d:
+            if k not in result:
+                result[k] = d[k]
+            else:
+                result[k] += d[k]
+                
 class Termo():
     
     def __init__(self,path_palavras=str):
         #Construtor que recebe o caminho para o arquivo txt com as palavras e adiciona todas elas
         #ao atributo words em formato de lista
-        self.words = []
+        self.untreatWords = []
         with open(path_palavras,'r') as text:
             for i in text:
-                self.words.append(i[:-1])
+                self.untreatWords.append(i[:-1])
     
     def filter(self,size=int):
         #Filtra self.words para abarcar somente palavras de tamanho size
 
         self.size = size
-        self.words = [i for i in self.words if len(i)==size and self.words]
+    
+        for word in self.untreatWords:
+            if '-' in word:
+                self.untreatWords.remove(word)
+          
+        self.words = [i for i in self.untreatWords if ((len(i)==size))]
+        self.words = list(set(self.words))
         
-
     def makeDic(self,index=int):
         #Devolve um dicionário com a chave sendo uma letra e o valor sendo a quantidade
         #de vezes que ela parece na posição index das palavras em self.words
 
         dic = {}
-        indic = {}
         for j in self.words:
             if j[index] not in dic:
                 dic[j[index]] = 1
@@ -56,25 +73,62 @@ class Termo():
         #uma lista das letra e suas pontuação como valor
          
         self.dicScoreTable = {}
+        self.scoreTable = {}
         for i in range(self.size):
-            self.dicScoreTable[i] = self.findBestLetter(i)    
-        #print(self.dicScoreTable)
+            #montar dicScoreTable com todas as posições como chaves e letras em ordem crescente de freq como valores
+            self.dicScoreTable[i] = self.findBestLetter(i)
         for i in self.dicScoreTable:
-            score = 10
-            for j in range(len(self.dicScoreTable[i])):
-                self.dicScoreTable[i][j] = [self.dicScoreTable[i][j],score]
-                if score != 0:
-                    score -= 1            
-                
-        return self.dicScoreTable
+            self.scoreTable[i] = {}
+            score = 20
+            for j in self.dicScoreTable[i]:
+                if j not in self.scoreTable[i]:
+                    self.scoreTable[i][j] = score
+                    if score != 0:
+                        score -= 1
+        return self.scoreTable
+    
     def assignScore(self):
-        scoredWords = []
+        self.scoredWords = {}
         for word in self.words:
-            score = 0
-            for letter in word:
-                pass
+                self.scoredWords[word] = 0
+        for index in range(self.size):
+            for word in self.words:
+                score = 0
+                if word[index] in self.scoreTable[index]:    
+                    score = self.scoreTable[index][word[index]]
+                self.scoredWords[word] += score
+        return self.scoredWords
+    
+    def findBestWord(self):
+        iscoredWords  = {}
+        for i,j in self.scoredWords.items():
+            #iscore = inverted score word
+            iscoredWords[j] = i
+        
+
+         
 
 test2 = Termo('Lista-de-Palavras.txt')
 test2.filter(5)
-print(test2.scoreTable())
-print('oi')
+freq0 = test2.makeDic(0)
+freq1 = test2.makeDic(1)
+freq2 = test2.makeDic(2)
+freq3 = test2.makeDic(3)
+freq4 = test2.makeDic(4)
+
+def merge_dicts(*dict_args):
+    """
+    Given any number of dictionaries, shallow copy and merge into a new dict,
+    precedence goes to key-value pairs in latter dictionaries.
+    """
+    result = {}
+    for d in dict_args:
+        for k in d:
+            if k not in result:
+                result[k] = d[k]
+            else:
+                result[k] += d[k]
+        
+    return result
+
+print(merge_dicts(freq0,freq1,freq2,freq3,freq4))
